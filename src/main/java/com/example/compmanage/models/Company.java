2810043c3f3f6834801253881d92ce18f3725a63
huynino96 <huynino0496@gmail.com>
2021-04-26 16:07:56 +0700

    Add Company Model and Fixed Cannot Run Server

--- src/main/java/com/example/compmanage/models/Company.java
+++ src/main/java/com/example/compmanage/models/Company.java
@@ -1,4 +1,38 @@
 package com.example.compmanage.models;
 
-public class Company {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Company extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String name;
+
+    public Company(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
