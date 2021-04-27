2922174a6a33ebd7f4d4ca080e8b78bd7200a06a
huynino96 <huynino0496@gmail.com>
2021-04-27 16:56:53 +0700

    Refactor Code

--- src/main/java/com/example/compmanage/security/SpringSecurityConfig.java
+++ src/main/java/com/example/compmanage/security/SpringSecurityConfig.java
@@ -1,14 +1,10 @@
 package com.example.compmanage.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
