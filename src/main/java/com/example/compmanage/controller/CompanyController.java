b99c217ec70981c6356dd4a29b9b47ffbb722a64
huynino96 <huynino0496@gmail.com>
2021-04-27 16:56:09 +0700

    Add GET and POST Controller and Spring Security

--- src/main/java/com/example/compmanage/controller/CompanyController.java
+++ src/main/java/com/example/compmanage/controller/CompanyController.java
@@ -1,4 +1,41 @@
 package com.example.compmanage.controller;
 
+import com.example.compmanage.exception.ResourceNotFoundException;
+import com.example.compmanage.models.Company;
+import com.example.compmanage.repository.CompanyRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class CompanyController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @GetMapping("/companies")
+    public List<Company> getCompany(){
+        return companyRepository.findAll();
+    }
+
+    @GetMapping("/companies/{id}")
+    public ResponseEntity< Company > getCompanyById(
+            @PathVariable(value = "id") Long companyId) throws ResourceNotFoundException {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new ResourceNotFoundException("Company not found :: " + companyId));
+        return ResponseEntity.ok().body(company);
+    }
+
+    @PostMapping("/companies")
+    public Company createCompany(@Valid @RequestBody Company company) {
+        return companyRepository.save(company);
+    }
+
+
+
 }
