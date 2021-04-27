bd4cefbe2083d2e8feae087d7f8c368db30676b5
huynino96 <huynino0496@gmail.com>
2021-04-27 19:48:52 +0700

    Create CPU Model

--- src/main/java/com/example/compmanage/models/CPU.java
+++ src/main/java/com/example/compmanage/models/CPU.java
@@ -1,4 +1,42 @@
 package com.example.compmanage.models;
 
-public class CPU {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "cpu")
+@Accessors(chain = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CPU implements Serializable {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="cpuType")
+    private String cpuType;
+
+    @Column(name = "coreType")
+    private String coreType;
+
+    @Column(name = "cpuGen")
+    private String cpuGen;
+
+    @Column(name= "cpuSpeed")
+    private String cpuSpeed;
+
+    @OneToMany(mappedBy = "cpu", cascade = {
+            CascadeType.ALL
+    })
+    private List<Computer> computer;
 }
--- src/main/java/com/example/compmanage/models/Computer.java
+++ src/main/java/com/example/compmanage/models/Computer.java
@@ -19,16 +19,23 @@ import java.time.LocalDateTime;
 @Accessors(chain = true)
 public class Computer implements Serializable {
     @Id
+    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "computerName")
     private String name;
+
+    @Column(name = "user")
     private String user;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "company_id")
     private Company company;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cpu_id")
+    private CPU cpu;
 
 
 }
