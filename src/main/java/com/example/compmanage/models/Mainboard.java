35d95b21ff7597782377bd021ad9b014b270bf87
huynino96 <huynino0496@gmail.com>
2021-04-27 19:57:04 +0700

    Create Mainboard Model

--- src/main/java/com/example/compmanage/models/Mainboard.java
+++ src/main/java/com/example/compmanage/models/Mainboard.java
@@ -1,4 +1,36 @@
 package com.example.compmanage.models;
 
-public class Mainboard {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "mainboard")
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Getter
+@Setter
+public class Mainboard implements Serializable {
+    @Id
+    @Column(name = "mainboard_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mainboardBrand")
+    private String mainboardBrand;
+
+    @Column(name = "mainboardName")
+    private String mainboardName;
+
+    @OneToMany(mappedBy = "mainboard", cascade = {
+            CascadeType.ALL
+    })
+    private List<Computer> computer;
 }
