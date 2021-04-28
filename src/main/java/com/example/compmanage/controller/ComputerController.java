fce668a985f7041800637affc0cb2d07c713bb2d
huynino96 <huynino0496@gmail.com>
2021-04-28 08:40:51 +0700

    Add Computer Controller

---
+++ src/main/java/com/example/compmanage/controller/ComputerController.java
@@ -0,0 +1,34 @@
+package com.example.compmanage.controller;
+
+import com.example.compmanage.exception.ResourceNotFoundException;
+import com.example.compmanage.models.Computer;
+import com.example.compmanage.repository.CompanyRepository;
+import com.example.compmanage.repository.ComputerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class ComputerController {
+    @Autowired
+    private ComputerRepository computerRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @GetMapping("/company/{companyId}/computer")
+    public List<Computer> getComputerByCompany(@PathVariable(value = "companyId") Long computerId) {
+        return computerRepository.findByCompanyId(computerId);
+    }
+
+    @PostMapping("/company/{companyId}/computer")
+    public Computer createComputer(@PathVariable(value = "companyId") Long companyId,
+                               @Valid @RequestBody Computer computer) throws ResourceNotFoundException {
+        return companyRepository.findById(companyId).map(company -> {
+                computer.setCompany(company);
+        return computerRepository.save(computer);
+        }).orElseThrow(() -> new ResourceNotFoundException("Company not found"));
+    }
+}
