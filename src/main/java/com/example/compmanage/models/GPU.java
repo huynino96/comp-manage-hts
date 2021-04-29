85211fd87abd4ae27d7406118cdbf98383e1f794
huynino96 <huynino0496@gmail.com>
2021-04-29 18:06:48 +0700

    Add GPU Model

--- src/main/java/com/example/compmanage/models/GPU.java
+++ src/main/java/com/example/compmanage/models/GPU.java
@@ -1,4 +1,41 @@
 package com.example.compmanage.models;
 
-public class GPU {
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "gpus")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GPU implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "gpuBrand")
+    private String gpuBrand;
+
+    @Column(name = "gpuName")
+    private String gpuName;
+
+    @Column(name = "gpuSize")
+    private String gpuSize;
+
+    @Column(name = "gpuType")
+    private String gpuType;
+
+    @OneToMany(mappedBy = "gpu", fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    @JsonManagedReference
+    private List<Computer> computer;
 }
