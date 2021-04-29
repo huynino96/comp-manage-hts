e0a1a2bcef47dfb22cd606186d8cd1a32908c905
huynino96 <huynino0496@gmail.com>
2021-04-29 21:18:55 +0700

    Finalize Computer Repository

--- src/main/java/com/example/compmanage/repository/ComputerRepository.java
+++ src/main/java/com/example/compmanage/repository/ComputerRepository.java
@@ -18,4 +18,23 @@ public interface ComputerRepository extends JpaRepository<Computer, Long> {
 
     List<Computer> findByDiskId(Long computerId);
     Optional<Computer> findByIdAndDiskId(Long id, Long diskId);
+
+    List<Computer> findByGpuId(Long computerId);
+    Optional<Computer> findByIdAndGpuId(Long id, Long gpuId);
+
+    List<Computer> findByKeyboardId(Long computerId);
+    Optional<Computer> findByIdAndKeyboardId(Long id, Long keyboardId);
+
+    List<Computer> findByMainboardId(Long computerId);
+    Optional<Computer> findByIdAndMainboardId(Long id, Long mainboardId);
+
+    List<Computer> findByMonitorId(Long computerId);
+    Optional<Computer> findByIdAndMonitorId(Long id, Long monitorId);
+
+    List<Computer> findByMouseId(Long computerId);
+    Optional<Computer> findByIdAndMouseId(Long id, Long mouseId);
+
+    List<Computer> findByRamId(Long computerId);
+    Optional<Computer> findByIdAndRamId(Long id, Long ramId);
+
 }
