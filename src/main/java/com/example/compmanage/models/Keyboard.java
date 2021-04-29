8d642012595c66a67e50d383147e1ad9337d52f6
huynino96 <huynino0496@gmail.com>
2021-04-29 18:11:55 +0700

    Add Keyboard Model

---
+++ src/main/java/com/example/compmanage/models/Keyboard.java
@@ -0,0 +1,31 @@
+package com.example.compmanage.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "keyboards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Keyboard implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "keyboardName")
+    private String keyboardName;
+
+    @OneToMany(mappedBy = "keyboard", fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    @JsonManagedReference
+    private List<Computer> computer;
+}
